37804622c86c6e21ff53a383772b21b6684256f8
alexandre devely <alexandre.devely@orange.com>
2021-03-04 16:44:50 +0100

    change path

--- create_zip.sh
+++ create_zip.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
-currpath=$(pwd)
 version=$(grep '"version"' src/manifest.json | grep -o "[0-9].[0-9].[0-9]")
 cd src
-zip -r -FS /tmp/abcdesktop-${version}.zip * --exclude *.git*
-cd $currpath
+zip -r -FS ../abcdesktop-${version}.zip * --exclude *.git*
